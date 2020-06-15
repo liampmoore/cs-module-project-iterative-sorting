210899a8c06f9c340d3e53bd7d5c6cebbcf8975a
Liam Moore <liam.moore.dev@gmail.com>
2020-06-15 22:05:32 +0000

    implements search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,9 @@
 def linear_search(arr, target):
     # Your code here
 
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +12,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    minimum = 0
+    maximum = len(arr) - 1
 
-
+    while minimum <= maximum:
+        middle = (maximum + minimum) // 2
+        # if the middle is less than the target get rid of everything less than the middle
+        if  arr[middle] < target:
+            minimum = middle + 1
+        # if the middle is greater than the target get rid of everything greater than the middle
+        elif arr[middle] > target:
+            maximum = middle - 1
+        # if the middle is the target return the index
+        else:
+            return middle
+        
     return -1  # not found
